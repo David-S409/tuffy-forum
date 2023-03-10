223b5538e07d3adfaced45725ef13b5f03d4b0d1
Daniel Avila <deadanny31@gmail.com>
2023-03-10 12:16:36 -0800

    testing frontend

---
+++ src/components/CourseList.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+interface Courses {
+    id: number;
+    name: string;
+}
+interface CourseListProps {
+  courses: Courses[];
+}
+
+const CourseList: React.FC<CourseListProps> = ({ courses }) => {
+  return (
+    <div>
+      <h2>Course List</h2>
+      <ul>
+        {courses.map((course) => (
+          <li key={course.id}>
+            <Link to={`/courses/${course.id}`}>{course.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CourseList;
