e27229fd5ea1ff89d3d62cddde2f0a12e43f13bb
Daniel Avila <deadanny31@gmail.com>
2023-03-12 12:43:49 -0700

    rendering the navigation bar in the home page and login button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,22 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { RootState } from "../store";
-import CourseList from "../components/CourseList";
-import Navigation from "../components/Navigation";
-import NewCourseForm from "../components/NewCourseForm";
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
+import LoginPage from './login/LoginPage';
+import Navigation from '../components/Navigation';
 
-const HomePage: React.FC = () => {
-  const { isAuth } = useSelector((state: RootState) => state.app);
+
+function Home() {
+  const user = useSelector((state: any) => state.app.user);
 
   return (
-    <div>
-        <Navigation />
-        <h1>Home Page</h1>
-        <NewCourseForm />
-        <CourseList courses={[{ id: 1, name: "Course 1"}]} />
-    </div>
-    );
-};
+    <>
+      <Navigation />
+      {user ? (
+        <h1> TUFFY FORUM {user.id} </h1>
+      ) : (
+        <LoginPage />
+      )}
+    </>
+  );
+}
 
-export default HomePage;
+export default Home;
