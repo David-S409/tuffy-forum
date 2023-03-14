aa44fcfdcbc1b3983d4cbfb8a83da95d4e25e023
Daniel Avila <deadanny31@gmail.com>
2023-03-13 17:31:42 -0700

    navigation bar updated render

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserAstronaut } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
-const Navigation = () => {
+function SearchBar (){
+  return (
+    <div>
+      <input type="text" placeholder="Search" />
+      <button>Search</button>
+    </div>
+  );
+};
+
+function UserProfile () {
+
+  const [showProfile, setShowProfile] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowProfile(true);
+  };
+  const handleMouseLeave = () => {
+    setShowProfile(false);
+  };
+
+  return (
+    <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        {/* <FontAwesomeIcon icon={faUserAstronaut} /> */}
+        <img src="https://brand.fullerton.edu/_assets/images/examples/Tuffy-Cheering.png" alt="user profile" />
+
+        {showProfile && (
+          <div className="user-profile">
+            <h3>Username</h3>
+            <h3>Member Since: </h3>
+            <h3>Year: </h3>
+            <h3>*Expert* </h3>
+            <h3>*Moderator* </h3>
+            <h3>Logout</h3>
+
+          </div>
+        )}
+
+        
+    </div>
+  );
+};
+
+function Navigation (){
   return (
     <nav>
       <ul>
@@ -10,6 +55,12 @@ const Navigation = () => {
         </h2>
         <h2>
           <Link to="/login">Login</Link>
+        </h2>
+        <h2>
+          <SearchBar />
+        </h2>
+        <h2>
+          <UserProfile />
         </h2>
       </ul>
     </nav>
