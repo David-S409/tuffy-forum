bde86b2406dccdf38a1ce23793e29a06c92a3000
KornTortilla <ryanagundez@gmail.com>
2023-04-17 19:40:55 -0700

    Prisma Updated for Cross Relation, Updated Course Endpoint
    Prisma Schema updated to have a cross relation between users and courses.
    
    Included endpoints for courses to search for course by name/course code, find course by id, post new course, and add course to users.

--- back-end/src/api/course/course.ts
+++ back-end/src/api/course/course.ts
@@ -31,4 +31,82 @@ router.get('/user/courses', isUserAuth, async (req, res) => {
   }
 });
 
+router.get('/courses/search', async (req, res) => {
+  const { query } = req;
+
+  if (Object.keys(query).length !== 0) {
+    const results = await prisma.course.findMany({
+      take: 10,
+      where: {
+        OR: [
+          { courseCode: { contains: query.courseCode as string } },
+          { name: { contains: query.name as string } },
+        ],
+      }
+    });
+
+    if (results) {
+      res.status(200).json(results);
+    }
+  } else {
+    res.status(400).json({ error: 'Invalid Search' });
+  }
+});
+
+router.get('/courses/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const course = await prisma.course.findUnique({
+    where: {
+      courseCode: String(id),
+    }
+  });
+
+  if (course) {
+    res.status(200).json(course);
+  } else {
+    res.status(404).json({ error: 'Question not found' });
+  }
+});
+
+router.post('/course', isUserAuth, async (req, res) => {
+  const { body } = req;
+
+  try {
+    await courseScheme.parseAsync(body);
+    const result = await prisma.course.create({
+      data: {
+        courseCode: body.courseCode,
+        name: body.name
+      },
+    });
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.post('/add/course/:id', isUserAuth, async (req, res) => {
+  const { id } = req.params;
+  const currentUser = req.user as User;
+
+  try {
+    const updatedCourse = await prisma.course.update({
+      where: { courseId: Number(id) },
+      data: { users: { create: { user: { connect: { userId: currentUser.userId } } } } },
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Error adding Course' });
+  }
+
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { userId: currentUser.userId },
+      data: { courses: { create: { course: { connect: { courseId: Number(id) } } } } },
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Error adding Course' });
+  }
+});
+
 export default router;
